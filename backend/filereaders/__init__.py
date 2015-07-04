5bfc6db28315d10802ebae7dd5cc9756eb31f9e0
Rob Flickenger <rob@hackerfriendly.com>
2015-07-04 16:40:24 -0700

    Tabs, not spaces.

--- backend/filereaders/__init__.py
+++ backend/filereaders/__init__.py
@@ -13,31 +13,31 @@ from .path_optimizers import optimize_all
 
 
 def read_svg(svg_string, target_size, tolerance, forced_dpi=None, optimize=True):
-    svgReader = SVGReader(tolerance, target_size)
-    parse_results = svgReader.parse(svg_string, forced_dpi)
-    if optimize:
-        optimize_all(parse_results['boundarys'], tolerance)
-    # {'boundarys':b, 'dpi':d, 'lasertags':l}
-    return parse_results
+	svgReader = SVGReader(tolerance, target_size)
+	parse_results = svgReader.parse(svg_string, forced_dpi)
+	if optimize:
+		optimize_all(parse_results['boundarys'], tolerance)
+	# {'boundarys':b, 'dpi':d, 'lasertags':l}
+	return parse_results
 
 
 def read_dxf(dxf_string, tolerance, optimize=True):
-    dxfReader = DXFReader(tolerance)
-    parse_results = dxfReader.parse(dxf_string)
-    if optimize:
-        optimize_all(parse_results['boundarys'], tolerance)
-    # # flip y-axis
-    # for color,paths in parse_results['boundarys'].items():
-    # 	for path in paths:
-    # 		for vertex in path:
-    # 			vertex[1] = 610-vertex[1]
-    return parse_results
+	dxfReader = DXFReader(tolerance)
+	parse_results = dxfReader.parse(dxf_string)
+	if optimize:
+		optimize_all(parse_results['boundarys'], tolerance)
+	# # flip y-axis
+	# for color,paths in parse_results['boundarys'].items():
+	# 	for path in paths:
+	# 		for vertex in path:
+	# 			vertex[1] = 610-vertex[1]
+	return parse_results
 
 
 def read_ngc(ngc_string, tolerance, optimize=True):
-    ngcReader = NGCReader(tolerance)
-    parse_results = ngcReader.parse(ngc_string)
-    # if optimize:
-    #     optimize_all(parse_results['boundarys'], tolerance)
-    return parse_results
+	ngcReader = NGCReader(tolerance)
+	parse_results = ngcReader.parse(ngc_string)
+	# if optimize:
+	#     optimize_all(parse_results['boundarys'], tolerance)
+	return parse_results
 
--- backend/filereaders/utilities.py
+++ backend/filereaders/utilities.py
@@ -24,20 +24,20 @@ def parseScalar(scalar_unit_string):
 
 
 def matrixMult(mA, mB):
-    return [ mA[0]*mB[0] + mA[2]*mB[1],
-                     mA[1]*mB[0] + mA[3]*mB[1],
-                     mA[0]*mB[2] + mA[2]*mB[3],
-                     mA[1]*mB[2] + mA[3]*mB[3],
-                     mA[0]*mB[4] + mA[2]*mB[5] + mA[4],
-                     mA[1]*mB[4] + mA[3]*mB[5] + mA[5] ]
+	return [ mA[0]*mB[0] + mA[2]*mB[1],
+					 mA[1]*mB[0] + mA[3]*mB[1],
+					 mA[0]*mB[2] + mA[2]*mB[3],
+					 mA[1]*mB[2] + mA[3]*mB[3],
+					 mA[0]*mB[4] + mA[2]*mB[5] + mA[4],
+					 mA[1]*mB[4] + mA[3]*mB[5] + mA[5] ]
 
 
 def matrixApply(mat, vec):
-    vec0 = mat[0]*vec[0] + mat[2]*vec[1] + mat[4]
-    vec[1] = mat[1]*vec[0] + mat[3]*vec[1] + mat[5]
-    vec[0] = vec0
+	vec0 = mat[0]*vec[0] + mat[2]*vec[1] + mat[4]
+	vec[1] = mat[1]*vec[0] + mat[3]*vec[1] + mat[5]
+	vec[0] = vec0
 
 
 def vertexScale(v, f):
-    v[0] *= f
-    v[1] *= f
+	v[0] *= f
+	v[1] *= f
